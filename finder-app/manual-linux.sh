92edef1a01b44ea6958fa742d96643b4b4f14bcc
Shashank C <shashank@Ubuntu20.myguest.virtualbox.org>
2023-02-05 03:08:36 -0700

    Assignment 3 Part 2 test

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -112,13 +112,12 @@ make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
-    cp finder.sh $OUTDIR/rootfs/home/
-    cp finder-test.sh $OUTDIR/rootfs/home/
-    cp writer $OUTDIR/rootfs/home/
-    cp autorun-qemu.sh $OUTDIR/rootfs/home/
-    mkdir $OUTDIR/rootfs/home/conf/
-    cp conf/* $OUTDIR/rootfs/home/conf/
+cp finder.sh $OUTDIR/rootfs/home/
+cp finder-test.sh $OUTDIR/rootfs/home/
+cp writer $OUTDIR/rootfs/home/
+cp autorun-qemu.sh $OUTDIR/rootfs/home/
+mkdir $OUTDIR/rootfs/home/conf/
+cp conf/* $OUTDIR/rootfs/home/conf/
    
 
 # TODO: Chown the root directory
