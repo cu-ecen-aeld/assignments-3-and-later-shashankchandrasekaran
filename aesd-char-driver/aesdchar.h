76d3db78b22a7fba407ff3263e286ca6db3d7c66
Shashank C <shashank@Ubuntu20.myguest.virtualbox.org>
2023-03-18 05:59:06 -0600

    Orignal driver code

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -30,15 +30,10 @@ struct aesd_dev
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
-    struct aesd_circular_buffer aesd_circular_buffer; /* Circular Buffer */
-
-    struct mutex lock;     /* mutual exclusion semaphore  used for write and read commands */
-
-    char *copy_buffer_ptr; /* Copy buffer Pointer used to store entries onto circular buffer*/
-
-    size_t buffer_size; /* Tracking size of copy buffer */
-
-    struct cdev cdev;     /* Char device structure      */
+  struct mutex lock;
+  struct aesd_buffer_entry *buffer_element;
+  struct aesd_circular_buffer circular_buffer;
+  struct cdev cdev;     /* Char device structure      */
 };
 
 
