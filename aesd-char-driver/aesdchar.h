08fd441a2fc962e3cd37bb76ea6afbe41eacf117
Shashank C <shashank@Ubuntu20.myguest.virtualbox.org>
2023-03-18 05:06:28 -0600

    Debugging driver

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -30,9 +30,9 @@ struct aesd_dev
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
+    struct aesd_circular_buffer cir_buff;
+    struct aesd_buffer_entry buff_entry;
     struct mutex lock;
-    struct aesd_buffer_entry *buffer_element;
-    struct aesd_circular_buffer circular_buffer;
     struct cdev cdev;     /* Char device structure      */
 };
 
